a215cbdc42f1fd44e680a2f59bfe2ab98357072f
ymew <yeon3191225@gmail.com>
2023-02-13 19:17:45 +0900

    구현

---
+++ game_development.py
@@ -0,0 +1,62 @@
+# 프로그램명: 게임 개발
+# 작성자: yeonmew
+# 작성일: 2023.02.13
+
+from timeit import default_timer as timer
+from datetime import timedelta
+
+
+N,M = map(int, input().split()) # N x M의 맵
+
+c = [[0] * M for _ in range(N)] # 방문 위치 저장을 위한 맵 생성
+
+x, y, direction = map(int,input().split()) # 현재 캐릭터의 x, y좌표와 방향 입력
+c[x][y] = 1 # 현 좌표 방문 처리
+
+arr = []
+for _ in range(N): #맵 입력
+    arr.append(list(map(int, input().split())))
+
+start_time = timer()
+
+dx = [-1,0,1,0] # 방향 정의
+dy = [0,1,0,-1]
+
+def turn_left(): #왼쪽으로 회전
+    global direction
+    direction -= 1 
+    if direction == -1:
+        direction = 3
+
+count = 1
+turn_count = 0
+
+while True:
+    turn_left()
+    cx = x + dx[direction] 
+    cy = y + dy[direction]
+
+    if c[cx][cy] == 0 and arr[cx][cy] == 0: # 회전 후 정면으로 갈 수 있는 경우
+        c[cx][cy] = 1
+        x = cx # 현재 위치 x를 이동 예정인 방향 cx로 이동
+        y = cy
+        count += 1
+        turn_count = 0
+        continue
+    else: # 회전 후 정면으로 갈 수 없는 경우
+        turn_count += 1
+
+    if turn_count == 4:
+        cx = x - dx[direction]
+        cy = y - dy[direction]
+        
+        if arr[cx][cy] == 0: # 뒤로 갈 수 있다면 이동
+            x = cx
+            y = cy
+        else: # 뒤로 이동할 수 없는 경우
+            break
+end_time = timer()
+
+print(count, timedelta(seconds= end_time - start_time))
+
+    
--- royal_knight.py
+++ royal_knight.py
@@ -1,9 +1,25 @@
-# 프로그램명: 커피 메뉴 수정 프로그램
+# 프로그램명: 왕실의 나이트
 # 작성자: yeonmew
-# 작성일: 2023.02.07
+# 작성일: 2023.02.12
 
-x,y  = input().split('')
+from timeit import default_timer as timer
+from datetime import timedelta
 
-if x == 'a':
-    x = 1
-print(x,",",y)
+print("좌표를 입력하세요.")
+coordinate = input()# 좌표를 입력받는다.
+
+start_time = timer()
+x = int(ord(coordinate[0])-(ord('a')))+1# a=1, b=2,,,로 만든다.
+y = int(coordinate[1])
+
+move = [(-1,-2),(-1,2),(1,2),(1,-2),(-2,-1),(-2,1),(2,-1),(2,1)] # 경우의 수를 입력해둔다.
+result = 0
+
+for moving in move:
+    x_move = x + moving[0]
+    y_move = y + moving[1]
+    if x_move >= 1 and x_move <= 8 and y_move >= 1 and y_move <= 8:
+        result += 1
+
+end_time = timer()
+print("경우의 수:",result, ", 걸린 시간:",timedelta(seconds= end_time - start_time))
