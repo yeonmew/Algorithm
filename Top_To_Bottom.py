6ee7d10ed9dccd9fd63681429f7168ae11f41b1e
ymew <yeon3191225@gmail.com>
2023-02-19 16:49:09 +0900

    update

--- Top_To_Bottom.py
+++ Top_To_Bottom.py
@@ -8,7 +8,7 @@ n = int(input())
 #N개의 정수를 입력받아 리스트에 저장
 arr = []
 for i in range(n):
-   arr.append(int(input))
+   arr.append(int(input()))
 #파이썬 기본 정렬 라이브러리를 이용하여 정렬 수행
 arr = sorted(arr, reverse = True)
 #정렬이 수행된 결과를 출력
